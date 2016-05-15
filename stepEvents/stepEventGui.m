ba1da7a52d88aa0bd4350b31dd92badf9f50264b
Hendrik Reimann <hendrikreimann@gmail.com>
2016-05-15 08:31:01 -0400

    cleaning up GUI function

--- stepEvents/stepEventGui.m
+++ stepEvents/stepEventGui.m
@@ -33,7 +33,7 @@ function stepEventGui(dataDirectory, trialToProcess)
     event_data = WalkingEventData(trial_data);
     
     % init gui
-    controller = createController;
+    controller = stepEventController(trial_data, event_data);
     
     % create position figures
     step_event_figure = createStepEventFigure('Positions Left');
@@ -77,20 +77,11 @@ function stepEventGui(dataDirectory, trialToProcess)
     linkaxes(step_event_axes, 'x');
     
     % load settings
-    loadFigureSettings();
-    findEvents();
+    controller.loadFigureSettings();
+    controller.findEvents();
     controller.setSelectedEvent();
 
     %% GUI
-    function controller = createController()
-        controller = stepEventController(trial_data, event_data);
-        set(controller.saveFigureSettingsButton, 'callback', @saveFigureSettings);
-        set(controller.loadFigureSettingsButton, 'callback', @loadFigureSettings);
-        set(controller.findEventsButton, 'callback', @findEvents);
-        set(controller.saveEventsButton, 'callback', @saveEvents);
-        
-        % move all these callbacks to the controller
-    end
     function step_event_figure = createStepEventFigure(title)
         step_event_figure = stepEventFigure(title, controller, trial_data, event_data);
         if strcmp(controller.figureSelectionBox.String, '<no figure>')
@@ -100,65 +91,11 @@ function stepEventGui(dataDirectory, trialToProcess)
             controller.figureSelectionBox.String = [controller.figureSelectionBox.String; {title}];
             controller.figureSelectionBox.UserData = [controller.figureSelectionBox.UserData; {step_event_figure}];
         end
+        
+        % this is not necessary, registration into the box can be done in the constructor
     end
 
-    function saveFigureSettings(varargin)
-        figure_settings = cell(1, length(controller.figureSelectionBox));
-        for i_figure = 1 : length(controller.figureSelectionBox.String)
-            figure_settings{i_figure} = controller.figureSelectionBox.UserData{i_figure}.getSetting();
-        end
-        control_figure_setting = struct();
-        control_figure_setting.position = controller.control_figure.Position;
-        
-        save_file = '/Users/reimajbi/Library/Application Support/stepEventFinder/figureSettings.mat';
-        save(save_file, 'figure_settings', 'control_figure_setting');
-    end
-    function loadFigureSettings(varargin)
-        load_file = '/Users/reimajbi/Library/Application Support/stepEventFinder/figureSettings.mat';
-        
-        load(load_file, 'figure_settings', 'control_figure_setting')
-        for i_figure = 1 : length(figure_settings)
-%             if length(step_event_figures) < i_figure
-%                 step_event_figures{i_figure} = createStepEventFigure();
-%             end
-            controller.figureSelectionBox.UserData{i_figure}.applySettings(figure_settings{i_figure});
-        end
-        controller.control_figure.Position = control_figure_setting.position;
-    end
 
-    %% events
-    function findEvents(varargin)
-        % left touchdown
-        [~, left_touchdown_indices_mocap] = findpeaks(-trial_data.getData('left_heel_z_pos'), 'MinPeakProminence', str2num(controller.heel_pos_peak_width.String));
-        time = trial_data.getTime('left_heel_z_pos');
-        left_touchdown_times = time(left_touchdown_indices_mocap);
-        event_data.setEventTimes(left_touchdown_times, 'left_touchdown');
-        
-        % left pushoff
-        [~, left_pushoff_indices_mocap] = findpeaks(trial_data.getData('left_toes_z_vel'), 'MinPeakProminence', str2num(controller.toes_vel_peak_width.String));
-        time = trial_data.getTime('left_toes_z_vel');
-        left_pushoff_times = time(left_pushoff_indices_mocap);
-        event_data.setEventTimes(left_pushoff_times, 'left_pushoff');
-        
-        % right touchdown
-        [~, right_touchdown_indices_mocap] = findpeaks(-trial_data.getData('right_heel_z_pos'), 'MinPeakProminence', str2num(controller.heel_pos_peak_width.String));
-        time = trial_data.getTime('right_heel_z_pos');
-        right_touchdown_times = time(right_touchdown_indices_mocap);
-        event_data.setEventTimes(right_touchdown_times, 'right_touchdown');
-
-        % right pushoff
-        [~, right_pushoff_indices_mocap] = findpeaks(trial_data.getData('right_toes_z_vel'), 'MinPeakProminence', str2num(controller.toes_vel_peak_width.String));
-        time = trial_data.getTime('right_toes_z_vel');
-        right_pushoff_times = time(right_pushoff_indices_mocap);
-        event_data.setEventTimes(right_pushoff_times, 'right_pushoff');
-
-        controller.updateEventPlots();
-
-
-    end
-    function saveEvents(varargin)
-        
-    end
 
     %% file system interaction
     function data = loadTrialData
