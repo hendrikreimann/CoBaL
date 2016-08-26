72707d30d00d54bc62a08732fdecf6edfc8fbab1
Hendrik Reimann <hendrikreimann@gmail.com>
2016-08-26 11:20:00 -0400

    forward dynamics - works with both feet, inverset dynamics works, but without respecting the measured GRF

--- groundReactionForceTest.m
+++ groundReactionForceTest.m
@@ -9,21 +9,23 @@ load subjectInfo.mat;
 model_file_name = makeFileName(date, subject_id, 'model');
 load(model_file_name);
 
-% plant.updateInternals;
+plant.updateInternals;
 
 
 % calculate constraints
 A_left = plant.bodyJacobians{3};
 A_left_dot = plant.bodyJacobianTemporalDerivatives{3};
-A_right = [];%plant.bodyJacobians{6};
-A_right_dot = [];%plant.bodyJacobianTemporalDerivatives{6};
+A_right = plant.bodyJacobians{6};
+A_right_dot = plant.bodyJacobianTemporalDerivatives{6};
+
+% A_left = [];
+% A_left_dot = [];
+
 number_of_left_foot_constraints = size(A_left, 1);
 number_of_right_foot_constraints = size(A_right, 1);
 
 A = [A_left; A_right];
 A_dot = [A_left_dot; A_right_dot];
-% A = A_left;
-% A_dot = A_left_dot;
 
 
 M = plant.inertiaMatrix;
@@ -32,18 +34,54 @@ N = plant.gravitationalTorqueMatrix;
 
 % find joint torques for zero accelerations
 P = eye(plant.numberOfJoints) - A' * (A * M^(-1) * A')^(-1) * A * M^(-1);
+
+% this next part is for zero workless torques
+virtual_joints = 1 : 6;
+k_c = rank(A);
+k_v = length(virtual_joints);
+[~, ~, V_c] = svd(A);
+C_c = V_c(:, k_c+1:end); % C_c spans the null space of A
+B_v = [eye(k_v); zeros(plant.numberOfJoints - k_v, k_v)];
+k_w = rank([B_v C_c]);
+[~, ~, V_w] = svd([B_v C_c]');
+C_w = V_w(:, k_w+1:end);
+% D = [B_v C_w];
+
+
 theta_two_dot_des = zeros(plant.numberOfJoints, 1);
+H = ...
+    [ ...
+      P; ...                                                    % desired joint acceleration
+      B_v'; ...                                                 % torques in the free body dofs
+      C_w'; ...                                                 % workless torques
+    ]; ...       
+    
 T = pinv ...
   ( ...
     [ ...
       P; ...                                                    % desired joint acceleration
-      [eye(6, 6), zeros(6, plant.numberOfJoints - 6)] ...       % torques in the free body dofs
+      B_v'; ...                                                 % torques in the free body dofs
+      C_w'; ...                                                 % workless torques
     ] ...       
   ) * ...
   [ ...
     P*C*plant.jointVelocities + P*N + M*theta_two_dot_des; ...  % desired joint acceleration
-    zeros(6, 1) ...                                             % torques in free dofs
+    zeros(6, 1); ...                                            % torques in free dofs
+    zeros(6, 1); ...                                            % workless torques
   ];
+
+% try applying the torque that keeps the body still for only the right foot constrained, but with both feet constrained
+% T = T_right_only;
+% this works, the accelerations are still zero, as they should be
+% but why do I not get a torque that actually gives me zero accelerations for both feet constrained?
+
+% try this without constraining the free DoFs
+% T = pinv ...
+%   ( ...
+%     P ...                                                    % desired joint acceleration
+%   ) * ...
+%     P*C*plant.jointVelocities + P*N + M*theta_two_dot_des; ...  % desired joint acceleration
+
 
 % apply constraints and check resulting joint accelerations
 lambda = (A*M^(-1)*A')^(-1) ...
@@ -52,6 +90,25 @@ constraint_torques = A'*lambda;
 
 theta_two_dot_res = M^(-1) * (T - C*plant.jointVelocities - N - constraint_torques);
 
+unconstrained_equation_of_motion_check_left = M*theta_two_dot_res + C*plant.jointVelocities + N + constraint_torques;
+unconstrained_equation_of_motion_check_right = T;
+constrained_equation_of_motion_check_left = M*theta_two_dot_res + P*(C*plant.jointVelocities + N);
+constrained_equation_of_motion_check_right = P*T;
+
+
+% there's some problem here, the resulting accelerations are not 0 when constraining both feet
+
+% maybe the matrix I am pseudo-inverting doesn't have full rank?
+H = [ ...
+      P; ...                                                    % desired joint acceleration
+      [eye(6, 6), zeros(6, plant.numberOfJoints - 6)] ...       % torques in the free body dofs
+    ];
+% yes, that is the right. With both feet constrained, rank(P) = 27, with one feet constrained rank(P) = 32
+
+% I suspect that this is because I have too many degrees of freedom. I can probably constrain this by not permitting
+% workless joints. But how do I do that?    
+
+% return
 
 % calculate ground reaction wrenches
 lambda_left = [lambda(1:number_of_left_foot_constraints); zeros(number_of_right_foot_constraints, 1)];
@@ -69,7 +126,8 @@ combined_ground_reaction_wrench = [left_ground_reaction_wrench; right_ground_rea
 
 left_ground_reaction_wrench_alt = calculateInstantaneousGroundReactionWrench_alt(plant, constraint_torque_left_alt, eye(4, 4), 12);
 right_ground_reaction_wrench_alt = calculateInstantaneousGroundReactionWrench_alt(plant, constraint_torque_right_alt, eye(4, 4), 18);
-combined_ground_reaction_wrench_alt = left_ground_reaction_wrench_alt;% + right_ground_reaction_wrench_alt;
+% combined_ground_reaction_wrench_alt = left_ground_reaction_wrench_alt + right_ground_reaction_wrench_alt;
+combined_ground_reaction_wrench_alt = right_ground_reaction_wrench_alt;
 
 
 
@@ -87,14 +145,17 @@ k_w = rank([B_v C_c]);
 C_w = V_w(:, k_w+1:end);
 D = [B_v C_w];
 
-% set up equation matrices
-R_left = pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 12)');
-H_1_left = R_left * A' * (A*M^(-1)*A')^(-1)*A*M^(-1);
-b_1_left = left_ground_reaction_wrench_alt - R_left * A' * (A*M^(-1)*A')^(-1) * (A*M^(-1)*(- C*plant.jointVelocities - N) + A_dot*plant.jointVelocities);
+% do not use the "no workless torques"-assumption
+D = B_v;
 
-% R_right = pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 18)');
-% H_1_right = R_right * A' * (A*M^(-1)*A')^(-1)*A*M^(-1);
-% b_1_right = right_ground_reaction_wrench_alt - R_right * A' * (A*M^(-1)*A')^(-1) * (A*M^(-1)*(- C*plant.jointVelocities - N) + A_dot*plant.jointVelocities);
+% set up equation matrices
+% R_left = pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 12)');
+% H_1_left = R_left * A' * (A*M^(-1)*A')^(-1)*A*M^(-1);
+% b_1_left = left_ground_reaction_wrench_alt - R_left * A' * (A*M^(-1)*A')^(-1) * (A*M^(-1)*(- C*plant.jointVelocities - N) + A_dot*plant.jointVelocities);
+
+R_right = pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 18)');
+H_1_right = R_right * A' * (A*M^(-1)*A')^(-1)*A*M^(-1);
+b_1_right = right_ground_reaction_wrench_alt - R_right * A' * (A*M^(-1)*A')^(-1) * (A*M^(-1)*(- C*plant.jointVelocities - N) + A_dot*plant.jointVelocities);
 
 % R_both = [pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 12)'); pinv(plant.calculateArbitraryFrameBodyJacobian(eye(4, 4), 18)')];
 % H_1_both = R_both * A' * (A*M^(-1)*A')^(-1)*A*M^(-1);
@@ -106,8 +167,8 @@ b_2 = theta_two_dot_res + M^(-1)*P*(C*plant.jointVelocities + N) + M^(-1)*A'*(A*
 H_3 = D';
 b_3 = zeros(size(D, 2), 1);
 
-H = [H_1_left; H_2; H_3];
-b = [b_1_left; b_2; b_3];
+% H = [H_1_left; H_2; H_3];
+% b = [b_1_left; b_2; b_3];
 
 % H = [H_1_both; H_2; H_3];
 % b = [b_1_both; b_2; b_3];
@@ -117,6 +178,8 @@ b = [b_2; b_3];
 
 F = pinv(H) * b;
 
+TF = [T F];
+TmF = T - F;
 % works for one contact foot, i.e. F = T, the joint torques calculated by inverse dynamics are the same as I applied
 
 
