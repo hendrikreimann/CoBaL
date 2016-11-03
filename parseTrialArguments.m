0204bb2452f6040fcb06a993c49cee938af0a617
Hendrik Reimann <hendrik.reimann@ini.rub.de>
2016-11-03 11:10:50 -0400

    made condition and trial parsing more flexible

--- parseTrialArguments.m
+++ parseTrialArguments.m
@@ -14,19 +14,42 @@ function [conditions_to_analyze, trials_to_analyze] = parseTrialArguments(vararg
     condition = parser.Results.condition;
     trials = parser.Results.trials;
     
+    % list of conditions
     if strcmp(condition, 'all')
         conditions_to_analyze = condition_list;
-        trials_to_analyze = trial_number_list;
     else
         conditions_to_analyze = {condition};
-        if trials == 0;
-            % find list of available trials for this condition
-            trials_to_analyze = trial_number_list(strcmp(condition_list, condition));
-        else
-            trials_to_analyze = {trials};
-        end
-            
     end
+    
+    % list of trials for each condition
+    if trials == 0;
+        % find list of available trials for this condition
+        trials_to_analyze = trial_number_list(strcmp(condition_list, condition));
+    else
+        trials_to_analyze = {trials};
+        % was there one list of trials given for several conditions?
+        if length(conditions_to_analyze) > 1 && length(trials_to_analyze) == 1
+            requested_trials = trials_to_analyze{1};
+            trials_to_analyze = cell(size(conditions_to_analyze));
+            for i_condition = 1 : length(conditions_to_analyze)
+                condition_label = conditions_to_analyze{i_condition};
+                condition_index_in_complete_list = find(strcmp(condition_list, condition_label));
+                trials_to_analyze_in_this_condition = [];
+                available_trials_in_this_condition = trial_number_list{condition_index_in_complete_list};
+                
+                
+                % only choose trials that are not present in this condition
+                for i_trial = 1 : length(requested_trials)
+                    if ismember(requested_trials(i_trial), available_trials_in_this_condition)
+                        trials_to_analyze_in_this_condition = [trials_to_analyze_in_this_condition requested_trials(i_trial)];
+                    end
+                end
+                trials_to_analyze{i_condition} = trials_to_analyze_in_this_condition;
+            end
+        end
+        
+    end
+    
     
     % exclude calibration
     trials_to_analyze(strcmp(conditions_to_analyze, 'calibration')) = [];
