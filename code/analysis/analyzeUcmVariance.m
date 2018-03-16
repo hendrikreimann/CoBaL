b4cf232ed631ed2d6f6e2d1a866f171331037109
Hendrik Reimann <hendrikreimann@gmail.com>
2018-03-16 11:06:51 -0400

    added analysis of discrete perturbation trials

--- code/analysis/analyzeUcmVariance.m
+++ code/analysis/analyzeUcmVariance.m
@@ -53,10 +53,17 @@ function analyzeUcmVariance(varargin)
     number_of_blocks = length(block_labels);
     
     % make containers to hold the data
-    V_para_session = zeros(number_of_ucm_variables, number_of_blocks);
-    V_perp_session = zeros(number_of_ucm_variables, number_of_blocks);
+%     V_para_session = zeros(number_of_ucm_variables, number_of_blocks);
+%     V_perp_session = zeros(number_of_ucm_variables, number_of_blocks);
+    subject_list = {};
+    condition_list = {};
+    time_point_list = {};
+    stretch_data_session = cell(number_of_ucm_variables, 1);
+    stretch_directions_session = cell(number_of_ucm_variables, 2);
+    [stretch_directions_session{:, :}] = deal('~');
+    stretch_names_session = ucm_variables;
     
-    
+    % calculate UCM variance measures
     for i_block = 1 : number_of_blocks
         this_block_label = block_labels{i_block};
         this_block_trials = trials_by_block{i_block};
@@ -77,7 +84,7 @@ function analyzeUcmVariance(varargin)
                 
                 % load indices
                 loaded_data = load(['analysis' filesep makeFileName(date, subject_id, trial_type, this_block_trials(i_trial), 'events.mat')]);
-                event_times = loaded_data.event_data{1}; % assume that we only have one kind of event
+                event_times = loaded_data.event_data{strcmp(loaded_data.event_labels, 'oscillation_peaks')};
                 event_indices_mocap = findClosestIndex(event_times, time_mocap);
                 
                 % calculate variance measures
@@ -101,20 +108,86 @@ function analyzeUcmVariance(varargin)
                     end
                     
                     % calculate variance measures
-                    [V_para, V_perp] = calculateUcmVariance(joint_angle_trajectories', jacobian);
+                    [V_para, V_perp] = calculateUcmVariance(joint_angle_data_to_analyze', jacobian);
                     V_para_trial(i_variable, i_trial) = V_para;
                     V_perp_trial(i_variable, i_trial) = V_perp;
                 end
             end
             % store data for this block as mean across trials
-            V_para_session(:, i_block) = mean(V_para_trial, 2);
-            V_perp_session(:, i_block) = mean(V_perp_trial, 2);
+            for i_variable = 1 : number_of_ucm_variables
+                V_para_this_block_this_variable = mean(V_para_trial(i_variable, :));
+                V_perp_this_block_this_variable = mean(V_perp_trial(i_variable, :));
+                
+                stretch_data_session{i_variable} = [stretch_data_session{i_variable} [V_para_this_block_this_variable; V_perp_this_block_this_variable]];
+            end
             
+            subject_list = [subject_list; subject_id]; %#ok<AGROW>
+            time_point_list = [time_point_list; 'N/A']; %#ok<AGROW>
+            condition_list = [condition_list; this_block_label]; %#ok<AGROW>
         end
         if any(strcmp(this_block_label, across_trials_conditions))
             % analyze across trials
-            for i_trial = this_block_trials
+            number_of_trials_this_block = length(this_block_trials);
+            expected_event_labels = {'perturbation_start';'perturbation_end';'perturbation_end_plus_one';'perturbation_end_plus_two'};
+            number_of_events = length(expected_event_labels);
+            joint_angle_data_to_analyze = cell(number_of_events, 1);
+            for i_trial = 1 : number_of_trials_this_block
+                % load data
+                loaded_data = load(['processed' filesep makeFileName(date, subject_id, trial_type, this_block_trials(i_trial), 'kinematicTrajectories.mat')]);
+                joint_angle_trajectories = loaded_data.joint_angle_trajectories;
+                time_mocap = loaded_data.time_mocap;
                 
+                % load and check events
+                loaded_data = load(['analysis' filesep makeFileName(date, subject_id, trial_type, this_block_trials(i_trial), 'events.mat')]);
+                if ~(length(loaded_data.event_data)==number_of_events)
+                    error(['Trial ' num2str(this_block_trials(i_trial)) ' - expected ' num2str(number_of_events) ' events, but found ' num2str(length(event_indices_mocap))]);
+                end
+                
+                % extract events
+                event_times = zeros(1, number_of_events);
+                for i_event = 1 : number_of_events
+                    if ~(any(strcmp(expected_event_labels{i_event}, loaded_data.event_labels)))
+                        error(['Trial ' num2str(this_block_trials(i_trial)) ' - event label "' expected_event_labels{i_event} '" not found']);
+                    end
+                    event_times(i_event) = loaded_data.event_data{strcmp(loaded_data.event_labels, expected_event_labels{i_event})};
+                end
+                event_indices_mocap = findClosestIndex(event_times, time_mocap);
+
+                % extract data
+                for i_event = 1 : number_of_events
+                    joint_angle_data_this_event = joint_angle_trajectories(event_indices_mocap(i_event), :)';
+                    joint_angle_data_to_analyze{i_event} = [joint_angle_data_to_analyze{i_event} joint_angle_data_this_event];
+                end
+            end
+            
+            % calculate and store variance measures
+            for i_event = 1 : number_of_events
+                joint_angle_data_to_analyze_this_event = joint_angle_data_to_analyze{i_event};
+                theta_mean = mean(joint_angle_data_to_analyze_this_event);
+                kinematic_tree.jointAngles = theta_mean;
+                kinematic_tree.updateConfiguration;
+                for i_variable = 1 : number_of_ucm_variables
+                    % calculate Jacobian
+                    if strcmp(ucm_variables{i_variable}, 'com_ap')
+                        J_com = kinematic_tree.calculateCenterOfMassJacobian;
+                        jacobian = J_com(1, :);
+                    end
+                    if strcmp(ucm_variables{i_variable}, 'com_vert')
+                        J_com = kinematic_tree.calculateCenterOfMassJacobian;
+                        jacobian = J_com(3, :);
+                    end
+                    if strcmp(ucm_variables{i_variable}, 'com_2d')
+                        J_com = kinematic_tree.calculateCenterOfMassJacobian;
+                        jacobian = J_com([1 3], :);
+                    end
+                    
+                    % calculate variance measures
+                    [V_para_this_block_this_variable, V_perp_this_block_this_variable] = calculateUcmVariance(joint_angle_data_to_analyze_this_event, jacobian);
+                    stretch_data_session{i_variable} = [stretch_data_session{i_variable} [V_para_this_block_this_variable; V_perp_this_block_this_variable]];
+                end
+                subject_list = [subject_list; subject_id]; %#ok<AGROW>
+                time_point_list = [time_point_list; expected_event_labels{i_event}]; %#ok<AGROW>
+                condition_list = [condition_list; this_block_label]; %#ok<AGROW>
             end
             
         end
@@ -122,19 +195,47 @@ function analyzeUcmVariance(varargin)
         
     end
     
+    number_of_stretches = length(subject_list);
+    origin_start_time_list_session = zeros(number_of_stretches, 1); % doesn't apply, but needs to be here for now
+    origin_end_time_list_session = zeros(number_of_stretches, 1); % doesn't apply, but needs to be here for now
+    origin_trial_list_session = zeros(number_of_stretches, 1); % doesn't apply, but needs to be here for now
+    time_list_session = zeros(number_of_stretches, 1); % doesn't apply, but needs to be here for now
     
-    
-    % save data
-    variable_names_session = ucm_variables; %#ok<NASGU>
+
+    %% save data
+    bands_per_stretch = 2;
+    conditions_session = struct;
+    conditions_session.subject_list = subject_list;
+    conditions_session.condition_list = condition_list;
+    conditions_session.time_point_list = time_point_list;
     results_file_name = ['analysis' filesep makeFileName(date, subject_id, 'results')];
     save ...
       ( ...
         results_file_name, ...
-        'V_para_session', ...
-        'V_perp_session', ...
-        'variable_names_session', ...
-        'block_labels' ...
+        'conditions_session', ...
+        'stretch_data_session', ...
+        'stretch_names_session', ...
+        'stretch_directions_session', ...
+        'bands_per_stretch', ...
+        'origin_trial_list_session', ...
+        'origin_start_time_list_session', ...
+        'origin_end_time_list_session', ...
+        'time_list_session' ...
       )
+    
+    
+    
+    % save data
+%     variable_names_session = ucm_variables; %#ok<NASGU>
+%     results_file_name = ['analysis' filesep makeFileName(date, subject_id, 'results')];
+%     save ...
+%       ( ...
+%         results_file_name, ...
+%         'V_para_session', ...
+%         'V_perp_session', ...
+%         'variable_names_session', ...
+%         'block_labels' ...
+%       )
 end
 
           
--- helper/checkArmsenseData.m
+++ helper/checkArmsenseData.m
@@ -4,18 +4,34 @@
 % go through all available folders with data and extract the file sizes
 folder_list = ...
   { ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/102/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/102/day2/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/103/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/103/day2/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/201/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/201/day2/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/203/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/203/day2/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/204/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/204/day2/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/207/day1/empty_ascii' ...
-    '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/207/day2/empty_ascii' ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/101/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/101/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/102/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/102/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/103/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/103/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/104/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/104/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/105/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/105/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/106/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/106/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/107/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/107/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/201/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/201/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/202/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/202/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/203/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/203/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/204/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/204/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/205/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/205/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/206/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/206/day2/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/207/day1/ascii', ...
+    '/Users/reimajbi/Drive_UD/ArmSense_intervention_VERIFIED/207/day2/ascii', ...
   };
 
 file_names_marker = {};
@@ -53,25 +69,25 @@ end
 
 %% gather candidates for duplicates depending on file size
 % go through lists and look for duplicates - marker
-% unique_file_sizes = unique(file_sizes_marker);
-% file_table_marker = cell(length(unique_file_sizes), 2);
-% file_table_marker(:, 1) = num2cell(unique_file_sizes);
-% for i_file = 1 : length(file_sizes_marker)
-%     this_file_name = file_names_marker{i_file};
-%     this_file_size = file_sizes_marker(i_file);
-%     this_file_srce = file_srces_marker{i_file};
-%     file_name_with_path = [this_file_srce filesep this_file_name];
-%     
-%     this_file_index = find(this_file_size==unique_file_sizes);
-%     file_table_marker{this_file_index, 2} = [file_table_marker{this_file_index, 2}; {file_name_with_path}];
-% end
-% % remove entries without duplicates
-% duplicate_counts_marker = zeros(size(file_table_marker, 1), 1);
-% for i_file = 1 : size(file_table_marker, 1)
-%     duplicate_counts_marker(i_file) = length(file_table_marker{i_file, 2});
-% end
-% file_table_marker(duplicate_counts_marker==1, :) = [];
-% duplicate_counts_marker(duplicate_counts_marker==1, :) = [];
+unique_file_sizes = unique(file_sizes_marker);
+file_table_marker = cell(length(unique_file_sizes), 2);
+file_table_marker(:, 1) = num2cell(unique_file_sizes);
+for i_file = 1 : length(file_sizes_marker)
+    this_file_name = file_names_marker{i_file};
+    this_file_size = file_sizes_marker(i_file);
+    this_file_srce = file_srces_marker{i_file};
+    file_name_with_path = [this_file_srce filesep this_file_name];
+    
+    this_file_index = find(this_file_size==unique_file_sizes);
+    file_table_marker{this_file_index, 2} = [file_table_marker{this_file_index, 2}; {file_name_with_path}];
+end
+% remove entries without duplicates
+duplicate_counts_marker = zeros(size(file_table_marker, 1), 1);
+for i_file = 1 : size(file_table_marker, 1)
+    duplicate_counts_marker(i_file) = length(file_table_marker{i_file, 2});
+end
+file_table_marker(duplicate_counts_marker==1, :) = [];
+duplicate_counts_marker(duplicate_counts_marker==1, :) = [];
 
 % go through lists and look for duplicates - analog
 unique_file_sizes = unique(file_sizes_analog);
@@ -95,53 +111,53 @@ file_table_analog(duplicate_counts_analog==1, :) = [];
 duplicate_counts_analog(duplicate_counts_analog==1, :) = [];
 
 %% check all candidates to see whether they're actually duplicates
-% file_table_marker = [file_table_marker cell(size(file_table_marker, 1), 1)];
-% for i_entry = 1 : size(file_table_marker, 1)
-%     % load all files and extract data from this entry
-%     this_file_name_list = file_table_marker{i_entry, 2};
-%     this_data = cell(length(this_file_name_list), 1);
-%     for i_file = 1 : length(this_file_name_list)
-%         this_file = this_file_name_list{i_file};
-%         if strcmp(this_file(end-2:end), 'tsv')
-%             delimiter = '\t';
-%             number_of_header_lines = 10;
-%         end
-%         if strcmp(this_file(end-2:end), 'csv')
-%             delimiter = ',';
-%             number_of_header_lines = 5;
-%         end
-%         [imported_data, delimiter, number_of_header_lines_returned] = importdata(this_file, delimiter, number_of_header_lines);
-%         this_data{i_file} = imported_data.data;
-%     end
-%     
-%     % compare each files against all others
-%     this_entry_match_percentages = cell(length(this_file_name_list), 1);
-%     for i_file = 1 : length(this_file_name_list)
-%         this_entry_match_percentages_row = zeros(1, length(this_file_name_list));
-%         
-%         this_file_data = this_data{i_file};
-%         this_file_data(isnan(this_file_data)) = 0;
-%         for j_file = 1 : length(this_file_name_list)
-%             that_file_data = this_data{j_file};
-%             that_file_data(isnan(that_file_data)) = 0;
-%             if numel(this_file_data) == numel(that_file_data)
-%                 number_of_matches = sum(sum(this_file_data == that_file_data));
-%                 match_ratio = number_of_matches / numel(that_file_data);
-%                 match_percentage = match_ratio * 100;
-%             else
-%                 match_percentage = 0;
-%             end
-%             this_entry_match_percentages_row(1, j_file) = match_percentage;
-%         end
-%         this_entry_match_percentages{i_file} = this_entry_match_percentages_row;
-%     end
-%     
-%     % store
-%     file_table_marker{i_entry, 3} = this_entry_match_percentages;
-% end
-% % save
-% file_name = '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/matches_marker.mat'
-% save(file_name, 'file_table_marker');
+file_table_marker = [file_table_marker cell(size(file_table_marker, 1), 1)];
+for i_entry = 1 : size(file_table_marker, 1)
+    % load all files and extract data from this entry
+    this_file_name_list = file_table_marker{i_entry, 2};
+    this_data = cell(length(this_file_name_list), 1);
+    for i_file = 1 : length(this_file_name_list)
+        this_file = this_file_name_list{i_file};
+        if strcmp(this_file(end-2:end), 'tsv')
+            delimiter = '\t';
+            number_of_header_lines = 10;
+        end
+        if strcmp(this_file(end-2:end), 'csv')
+            delimiter = ',';
+            number_of_header_lines = 5;
+        end
+        [imported_data, delimiter, number_of_header_lines_returned] = importdata(this_file, delimiter, number_of_header_lines);
+        this_data{i_file} = imported_data.data;
+    end
+    
+    % compare each files against all others
+    this_entry_match_percentages = cell(length(this_file_name_list), 1);
+    for i_file = 1 : length(this_file_name_list)
+        this_entry_match_percentages_row = zeros(1, length(this_file_name_list));
+        
+        this_file_data = this_data{i_file};
+        this_file_data(isnan(this_file_data)) = 0;
+        for j_file = 1 : length(this_file_name_list)
+            that_file_data = this_data{j_file};
+            that_file_data(isnan(that_file_data)) = 0;
+            if numel(this_file_data) == numel(that_file_data)
+                number_of_matches = sum(sum(this_file_data == that_file_data));
+                match_ratio = number_of_matches / numel(that_file_data);
+                match_percentage = match_ratio * 100;
+            else
+                match_percentage = 0;
+            end
+            this_entry_match_percentages_row(1, j_file) = match_percentage;
+        end
+        this_entry_match_percentages{i_file} = this_entry_match_percentages_row;
+    end
+    
+    % store
+    file_table_marker{i_entry, 3} = this_entry_match_percentages;
+end
+% save
+file_name = '/Users/reimajbi/Desktop/matches_marker.mat'
+save(file_name, 'file_table_marker');
 
 %%
 file_table_analog = [file_table_analog cell(size(file_table_analog, 1), 1)];
@@ -190,7 +206,7 @@ for i_entry = 1 : size(file_table_analog, 1)
 end
 
 % save
-file_name = '/Users/reimajbi/Downloads/Armsense_New_Upload_Check/matches_analog.mat';
+file_name = '/Users/reimajbi/Desktop/matches_analog.mat';
 save(file_name, 'file_table_analog');
 
 
