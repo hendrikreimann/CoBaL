e8fca9065a3532fd7198237c9185aa1a09840726
Tyler Fettrow <tfettrow@gmail.com>
2018-04-23 16:45:57 -0400

    deleted some uneeded scripts and added some plots (rename exploration script)

--- explore/test_continuous_individuals.m
+++ explore/test_continuous_individuals.m
@@ -10,64 +10,118 @@ subjects = {'DJB';'DXT';'EFU';'FNA';'GHJ';'IDA';'MTB';'NGY';'ONT';'PAG';'RON';'R
 
 for i_subject = 1:length(subjects)
 subject_indicator = ismember(conditions.subject_list,subjects(i_subject));
-
-% What does this script do?
-    % meant to grab data depending on what you want to look at
-    % 1) look at group data
-    % 2) look at all mechanism data
-    % 3) identify index 2
-    % 4) index 3?
-    % 5) all data should be inverted, so not necessary to decipher between
-    % right and left, just plot all
     
     % get condition descriptors
     stimulus_condition = conditions_to_test(i_condition, :);
     control_condition = conditions_control(strcmp(conditions_control(:, 1), stimulus_condition{1}), :);
+      
+    index_indicator = strcmp(conditions.condition_index_list, 'ONE');
+    stance_foot_indicator_left = strcmp(conditions.condition_stance_foot_list, 'STANCE_LEFT');
+    stance_foot_indicator_right = strcmp(conditions.condition_stance_foot_list, 'STANCE_RIGHT');
+    index_indicator_left = stance_foot_indicator_left & index_indicator;
+    index_indicator_right = stance_foot_indicator_right & index_indicator;
     
-    % get condition indicators
+    group_indicator_early = strcmp(conditions.condition_group_list,'Early');
+    group_indicator_late = strcmp(conditions.condition_group_list, 'Late');
+    group_indicator_no = strcmp(conditions.condition_group_list, 'None');
     
-    stance_foot_indicator_stimulus = strcmp(condition_stance_foot_list, stimulus_condition{1});
-    stance_foot_indicator_control = strcmp(condition_stance_foot_list, control_condition{1});
-    perturbation_indicator_stim_Right = strcmp(condition_perturbation_list, 'ILLUSION_RIGHT');
-    perturbation_indicator_stim_Left = strcmp(condition_perturbation_list, 'ILLUSION_LEFT');
-    perturbation_indicator_control = strcmp(condition_perturbation_list, control_condition{2});
-    index_indicator_stim = strcmp(condition_index_list, stimulus_condition{4});
-    index_indicator_control = strcmp(condition_index_list, control_condition{4});
-    indicator_stimulus_right = stance_foot_indicator_stimulus & perturbation_indicator_stim_Right & index_indicator_stim & subject_indicator;
-    indicator_stimulus_left = stance_foot_indicator_stimulus & perturbation_indicator_stim_Left & index_indicator_stim & subject_indicator;
-    indicator_control = stance_foot_indicator_control & perturbation_indicator_control & index_indicator_control & subject_indicator;
+    index_indicator_early = index_indicator & group_indicator_early;
+    index_indicator_late = index_indicator & group_indicator_late;
+    index_indicator_no = index_indicator & group_indicator_no;
     
+   
     % get data and perform t-tests
     for i_variable = 1 : number_of_variables_to_test
-        this_variable = variables_to_test{i_variable};
-        this_condition = conditions_to_test(i_condition, :);
+        
+        % TO DO: check cop_inverted
         
         if plot_cop_step
-            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_step_end_inverted'))}, variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))},'*');
+            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_step_end_inverted'))}(:,index_indicator), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator),'*');
         end
         
         if plot_icop_step
-            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_integrated_inverted'))}, variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))},'*');
+            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_integrated_inverted'))}(:,index_indicator), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator),'*');
         end
         
         if plot_i2cop_step
-            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_integrated_twice_inverted'))}, variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))},'*');
+            plot(variable_data{find(strcmp(variable_names, 'cop_from_com_x_integrated_twice_inverted'))}(:,index_indicator), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator),'*');
         end
         
-%         if plot_pushoff_step
-%             plot(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_max'))}, variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))},'*');
-%         end
         if plot_pushoff_step
-            plot(variable_data{find(strcmp(variable_names, 'left_ankle_dorsiflexion_step_end_inverted'))}(:,find(strcmp(conditions.condition_stance_foot_list, 'STANCE_LEFT'))), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,find(strcmp(conditions.condition_stance_foot_list, 'STANCE_LEFT'))),'*');
+            plot(variable_data{find(strcmp(variable_names, 'left_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator_left), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_left),'*');
             hold on;
-            plot(variable_data{find(strcmp(variable_names, 'right_ankle_dorsiflexion_step_end_inverted'))}(:,find(strcmp(conditions.condition_stance_foot_list, 'STANCE_RIGHT'))), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,find(strcmp(conditions.condition_stance_foot_list, 'STANCE_RIGHT'))),'*');
+            plot(variable_data{find(strcmp(variable_names, 'right_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator_right), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_right),'*');
         end
         
-         if plot_pushoff_step
-            plot(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_step_end_inverted'))}, variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))},'*');
+        if plot_pushoff_step
+            plot(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator), variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator),'*');
         end
         
         
+        % TO DO: add cinvs
+        
+        if plot_group_cop
+            figure; hold on;
+            plot(mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_inverted'))}(:,index_indicator_no),2));
+        end
+        
+        if plot_group_step
+            figure; hold on;
+            plot(mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_no),2));
+        end
+        
+        if plot_group_pushoff
+            plot(mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_inverted'))}(:,index_indicator_no),2));
+        end
+        
+        if plot_group_com
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_inverted'))}(:,index_indicator_no),2));
+        end
+
+        if plot_group_com_init
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_from_com_init_x_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_from_com_init_x_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_from_com_init_x_inverted'))}(:,index_indicator_no),2));
+        end
+        
+        if plot_group_com_vel
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_vel_inverted'))}(:,index_indicator_early),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_vel_inverted'))}(:,index_indicator_late),2));
+            plot(mean(variable_data{find(strcmp(variable_names, 'com_x_vel_inverted'))}(:,index_indicator_no),2));
+        end
+        
+        if bar_group_cop
+            figure; hold on;
+            cop_means = [mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_step_end_inverted'))}(:,index_indicator_early),2), ...
+                mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_step_end_inverted'))}(:,index_indicator_late),2), ...
+            mean(variable_data{find(strcmp(variable_names, 'cop_from_com_x_step_end_inverted'))}(:,index_indicator_no),2)];
+            bar(cop_means);   
+        end
+        
+        if bar_group_step
+            figure; hold on;
+            step_means = [mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_early),2), ...
+                mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_late),2), ...
+            mean(variable_data{find(strcmp(variable_names, 'step_placement_x_inverted'))}(:,index_indicator_no),2)];
+            bar(step_means);   
+        end
+        
+        if bar_group_pushoff
+            figure; hold on;
+            pushoff_means = [mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator_early),2), ...
+                mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator_late),2), ...
+            mean(variable_data{find(strcmp(variable_names, 'trigger_leg_ankle_dorsiflexion_step_end_inverted'))}(:,index_indicator_no),2)];
+            bar(pushoff_means);   
+        end
+
     end
 end
 
