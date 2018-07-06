3eef95f16e3eac4ac0a034532ac84959ae52ac93
Tyler Fettrow <tfettrow@gmail.com>
2018-07-06 10:54:26 -0400

    some changes made a few weeks ago, just never pushed.

--- code/eventHandling/determineStretchesToAnalyze.m
+++ code/eventHandling/determineStretchesToAnalyze.m
@@ -86,14 +86,14 @@ function determineStretchesToAnalyze(varargin)
         for i_trial = trials_to_process
             %% load data
             ignore_times = [];
-            load(['analysis' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'events')]);
-%             load(['analysis' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'stepEvents')]);
+%             load(['analysis' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'events')]);
+            load(['analysis' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'stepEvents')]);
 %             load(['processed' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'kinematicTrajectories')]);
             
-            right_pushoff_times = event_data{strcmp(event_labels, 'right_pushoff')};
-            right_touchdown_times = event_data{strcmp(event_labels, 'right_touchdown')};
-            left_pushoff_times = event_data{strcmp(event_labels, 'left_pushoff')};
-            left_touchdown_times = event_data{strcmp(event_labels, 'left_touchdown')};
+%             right_pushoff_times = event_data{strcmp(event_labels, 'right_pushoff')};
+%             right_touchdown_times = event_data{strcmp(event_labels, 'right_touchdown')};
+%             left_pushoff_times = event_data{strcmp(event_labels, 'left_pushoff')};
+%             left_touchdown_times = event_data{strcmp(event_labels, 'left_touchdown')};
 
             % determine experimental condition
             this_trial_type = condition_list{i_condition};
@@ -150,9 +150,11 @@ function determineStretchesToAnalyze(varargin)
             
             % stimulus data
             if strcmp(condition_stimulus, 'GVS_old')
-                GVS_out_trajectory = loadData(date, subject_id, condition_list{i_condition}, i_trial, 'GVS_out_trajectory');
+                load(['processed' filesep makeFileName(date, subject_id, condition_list{i_condition}, i_trial, 'labviewData')]);
+%                 GVS_out_trajectory = loadData(date, subject_id, condition_list{i_condition}, i_trial, 'GVS_out_trajectory');
                 GVS_stim_trajectory = GVS_out_trajectory + subject_settings.get('gvs_offset');
-                [stimulus_state_trajectory, time_stimulus] = loadData(date, subject_id, condition_list{i_condition}, i_trial, 'stimulus_state_trajectory');
+%                 [stimulus_state_trajectory, time_stimulus] = loadData(date, subject_id, condition_list{i_condition}, i_trial, 'stimulus_state_trajectory');
+                time_stimulus = time;
             end
             if strcmp(condition_stimulus, 'VISUAL')
                 % this if for TU data
@@ -1636,11 +1638,11 @@ function determineStretchesToAnalyze(varargin)
             end
             
             % add subject
-            subject_list = cell(size(event_variables_to_save.stance_foot_data, 1), 1);
-            for i_stretch = 1 : length(subject_list)
-                subject_list{i_stretch} = subject_id;
+            condition_subject_list = cell(size(event_variables_to_save.stance_foot_data, 1), 1);
+            for i_stretch = 1 : length(condition_subject_list)
+                condition_subject_list{i_stretch} = subject_id;
             end
-            conditions_trial.subject_list = subject_list;
+            conditions_trial.subject_list = condition_subject_list;
 
             %% remove stretches where important variables are missing
 
