8325b5fe9ce00d218ead8515f298065d16e84617
Hendrik Reimann <hendrikreimann@gmail.com>
2018-07-20 11:32:34 -0400

    test update

--- code/dataHandling/importTrc.m
+++ code/dataHandling/importTrc.m
@@ -1,5 +1,5 @@
 %     This file is part of the CoBaL code base
-%     Copyright (C) 2017 Hendrik Reimann <hendrikreimann@gmail.com>
+%     Copyright (C) 2018 Hendrik Reimann <hendrikreimann@gmail.com>
 % 
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
