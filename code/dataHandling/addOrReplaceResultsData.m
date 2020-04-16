ae5609d9e9253787664b24c7c4b859b20f8be627
Hendrik Reimann <hendrikreimann@gmail.com>
2020-04-16 12:25:19 +0200

    cleaning out processAnalysisVariables 3 -- finished move to use struct to store data

--- code/dataHandling/addOrReplaceResultsData.m
+++ code/dataHandling/addOrReplaceResultsData.m
@@ -14,15 +14,15 @@
 %     You should have received a copy of the GNU General Public License
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [data, names, directions] = addOrReplaceResultsData(data, names, directions, new_data, new_name, new_directions)
-    index_in_existing_data = find(strcmp(names, new_name));
+function data_struct = addOrReplaceResultsData(data_struct, new_data, new_name, new_directions)
+    index_in_existing_data = find(strcmp(data_struct.names, new_name));
     if isempty(index_in_existing_data)
-        data = [data; new_data];
-        names = [names; new_name];
-        directions = [directions; new_directions];
+        data_struct.data = [data_struct.data; new_data];
+        data_struct.names = [data_struct.names; new_name];
+        data_struct.directions = [data_struct.directions; new_directions];
     else
-        data{index_in_existing_data} = new_data;
-        directions(index_in_existing_data,:) = new_directions;
+        data_struct.data{index_in_existing_data} = new_data;
+        data_struct.directions(index_in_existing_data,:) = new_directions;
         % TODO: check whether directions are different
     end
 end
