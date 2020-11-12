81cda91a9206638154440ca0364f07dc3f158e6e
Hendrik Reimann <hendrikreimann@gmail.com>
2020-11-12 10:03:49 +0100

    edited spacing and line breaks to adhere to 120 char line limit

--- code/conditionHandling/determineConditionLevels.m
+++ code/conditionHandling/determineConditionLevels.m
@@ -14,7 +14,8 @@
 %     You should have received a copy of the GNU General Public License
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels(study_settings, subject_settings, trial_data)
+function [conditions_trial, event_variables_to_save, removal_flags] ...
+    = determineConditionLevels(study_settings, subject_settings, trial_data)
     experimental_paradigm = study_settings.get('experimental_paradigm');
     subject_id = subject_settings.get('subject_id');
     gender = subject_settings.get('gender');
@@ -25,46 +26,62 @@ function [conditions_trial, event_variables_to_save, removal_flags] = determineC
     removal_flags = zeros(size(trial_data.trigger_times));
     
     % determine levels particular to the experimental paradigm
-    paradigms_with_intermittent_perturbation = {'Vision', 'CadenceVision', 'GVS', 'CadenceGVS', 'FatigueGVS', 'OculusLaneRestriction', 'CognitiveLoadVision', 'CognitiveLoadGvs', 'SR_VisualStim'};
+    paradigms_with_intermittent_perturbation = ...
+      { ...
+        'Vision', 'CadenceVision', 'GVS', 'CadenceGVS', 'FatigueGVS', ...
+        'OculusLaneRestriction', 'CognitiveLoadVision', 'CognitiveLoadGvs', 'SR_VisualStim' ...
+      };
     if any(strcmp(experimental_paradigm, paradigms_with_intermittent_perturbation))
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_intermittentPerturbations(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_intermittentPerturbations(study_settings, subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'Normal Walking')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_normalWalking(subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_normalWalking(subject_settings, trial_data);
     end    
     if strcmp(experimental_paradigm, 'Stochastic Resonance')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_stochasticResonance(subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_stochasticResonance(subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'Normal Walking nGVS')
-        [conditions_trial_normal, event_variables_to_save_normal, removal_flags_normal] = determineConditionLevels_normalWalking(subject_settings, trial_data);
-        [conditions_trial_ngvs, event_variables_to_save_ngvs, removal_flags_ngvs] = determineConditionLevels_Ngvs(subject_settings, trial_data);
+        [conditions_trial_normal, event_variables_to_save_normal, removal_flags_normal] ...
+            = determineConditionLevels_normalWalking(subject_settings, trial_data);
+        [conditions_trial_ngvs, event_variables_to_save_ngvs, removal_flags_ngvs] ...
+            = determineConditionLevels_Ngvs(subject_settings, trial_data);
         
         conditions_trial = mergeConditionStruct(conditions_trial_normal, conditions_trial_ngvs);
         event_variables_to_save = mergeConditionStruct(event_variables_to_save_normal, event_variables_to_save_ngvs);
         removal_flags = removal_flags_normal & removal_flags_ngvs;
     end
     if strcmp(experimental_paradigm, 'nGVS_Vision')
-        [conditions_trial_vision, event_variables_to_save_vision, removal_flags_vision] = determineConditionLevels_intermittentPerturbations(study_settings, subject_settings, trial_data);
-        [conditions_trial_ngvs, event_variables_to_save_ngvs, removal_flags_ngvs] = determineConditionLevels_Ngvs(subject_settings, trial_data);
+        [conditions_trial_vision, event_variables_to_save_vision, removal_flags_vision] ...
+            = determineConditionLevels_intermittentPerturbations(study_settings, subject_settings, trial_data);
+        [conditions_trial_ngvs, event_variables_to_save_ngvs, removal_flags_ngvs] ...
+            = determineConditionLevels_Ngvs(subject_settings, trial_data);
         
         conditions_trial = mergeConditionStruct(conditions_trial_vision, conditions_trial_ngvs);
         event_variables_to_save = mergeConditionStruct(event_variables_to_save_vision, event_variables_to_save_ngvs);
         removal_flags = removal_flags_vision & removal_flags_ngvs;
     end
     if strcmp(experimental_paradigm, 'Vision_old')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_visionOld(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_visionOld(study_settings, subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'GVS_old')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_gvsOld(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_gvsOld(study_settings, subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'GvsOverground')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_gvsOverground(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_gvsOverground(study_settings, subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'GaitInitiationObstacle')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_gaitInitiationObstacle(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_gaitInitiationObstacle(study_settings, subject_settings, trial_data);
     end
     if strcmp(experimental_paradigm, 'Vision Stochastic')
-        [conditions_trial, event_variables_to_save, removal_flags] = determineConditionLevels_visionStochastic(study_settings, subject_settings, trial_data);
+        [conditions_trial, event_variables_to_save, removal_flags] ...
+            = determineConditionLevels_visionStochastic(study_settings, subject_settings, trial_data);
     end
     
     % add levels that are the same for all experimental paradigms
